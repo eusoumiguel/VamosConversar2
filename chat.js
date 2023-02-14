5d7d250f0cea37950a6714dba7dfbb4d590f3d56
eusoumiguel <113737239+eusoumiguel@users.noreply.github.com>
2023-02-14 19:34:17 -0300

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,33 @@
+// Configuração do Firebase de seu aplicativo
+//ADICIONE OS LINKS DO FIREBASE AQUI
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC6MBk2ubp4eUPEa-ihyqd1eCx32MrBPiU",
+    authDomain: "vamos-conversar-67c75.firebaseapp.com",
+    projectId: "vamos-conversar-67c75",
+    storageBucket: "vamos-conversar-67c75.appspot.com",
+    messagingSenderId: "923349974479",
+    appId: "1:923349974479:web:fd4e08cc650724682ca6ba"
+  };
+
+// Inicialize o Firebase
+firebase.initializeApp(firebaseConfig);
+
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    firebase.database().ref("/").child(user_name).update({
+        purpose : "adding user"
+    });
+    /*
+    Armazene o nome de usuário em localStorage
+
+    Programe window.location para que o usuário possa navegar até a página chat_room.html.
+    */
+   localStorage.setItem("user_name", user_name);
+
+   window.location = "chat_room.html";
+}
+
+
+
